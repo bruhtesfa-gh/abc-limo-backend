55b443c438f5da866e7e7091cc4374ae7deac6c2
Bruhtesfa Zelealem <61186942+bruhtesfa-gh@users.noreply.github.com>
2023-08-17 10:30:23 +0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,9 @@ app.use(
       "https://seattle-limos.onrender.com",
       "http://localhost",
       "127.0.0.1"
+      "https://abclimo1.netlify.app" ,
+      "https://abclimo2.netlify.app" ,
+      "https://abclimo3.netlify.app" ,
     ],
   })
 );
